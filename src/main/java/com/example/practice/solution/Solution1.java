c245940f76a3538e9237b71a93bd365c8b1e42a5
girl5hhhhh <lichunyanjy2020.163.com>
2023-05-13 11:39:05 +0800

    修改

--- src/main/java/com/example/practice/solution/Solution1.java
+++ src/main/java/com/example/practice/solution/Solution1.java
@@ -146,7 +146,7 @@ public class Solution1 {
     }
 */
 
-    public static int[] intersect(int[] nums1, int[] nums2) {
+    /*public static int[] intersect(int[] nums1, int[] nums2) {
         List<Integer> collect1 = Arrays.stream(nums1).boxed().collect(Collectors.toList());
         List<Integer> collect2 = Arrays.stream(nums2).boxed().collect(Collectors.toList());
         List<Integer> collect3 = collect1.stream().filter(e -> collect2.contains(e)).collect(Collectors.toList());
@@ -156,5 +156,39 @@ public class Solution1 {
         }
         System.out.println(resultNum);
         return resultNum;
+    }*/
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        // 先对两个数组进行排序
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        List<Integer> list = new ArrayList<>();
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                // 如果i指向的值小于j指向的值，，说明i指向
+                // 的值小了，i往后移一步
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                // 如果i指向的值大于j指向的值，说明j指向的值
+                // 小了，j往后移一步
+                j++;
+            } else {
+                // 如果i和j指向的值相同，说明这两个值是重复的，
+                // 把他加入到集合list中，然后i和j同时都往后移一步
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        //把list转化为数组
+        int index = 0;
+        int[] res = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            res[index++] = list.get(k);
+        }
+        System.out.println(res);
+        return res;
     }
 }
